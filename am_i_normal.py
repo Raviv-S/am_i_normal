f6bac83ceb64b78693b4dc12a960b9296e65e700
Raviv S <raviv.schaffer@gmail.com>
2020-04-29 20:41:09 +0300

    Finished assignment C

--- am_i_normal.py
+++ am_i_normal.py
@@ -12,5 +12,9 @@ print(get_above_avarage(range(10)))
 remove_patrick = lambda sentence: ''.join([letter for letter in sentence if letter not in 'patrickPATRICK'])
 print(remove_patrick('patrickRavivSchafferStar'))
 
-
+# C.
+remove_generic_numbers = lambda inp_nums: [int(str_rmv_num) if str_rmv_num != '' else 0 for str_rmv_num in
+                                           [str_num.replace('5', '').replace('7', '') for str_num in
+                                           [str(num) for num in inp_nums]]]
+print(remove_generic_numbers([75, 745, 3575753]))
 
