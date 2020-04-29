beff8011f35d4ea5a29670ee69cdc039daf013a7
Raviv S <raviv.schaffer@gmail.com>
2020-04-29 20:28:08 +0300

    Finished assignment B

--- am_i_normal.py
+++ am_i_normal.py
@@ -8,5 +8,9 @@ print(get_above_avarage(range(10)))
 get_above_avarage = lambda numbers: [a_num for a_num in numbers if a_num > sum(numbers) / len(numbers)]
 print(get_above_avarage(range(10)))
 
-
 # B.
+remove_patrick = lambda sentence: ''.join([letter for letter in sentence if letter not in 'patrickPATRICK'])
+print(remove_patrick('patrickRavivSchafferStar'))
+
+
+
